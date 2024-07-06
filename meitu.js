1c8db31d87e78e4a49833ef31ab85798f02f968a
toannxdev <72689204+toannxdev@users.noreply.github.com>
2024-07-06 23:43:23 +0700

    Update meitu.js

--- meitu.js
+++ meitu.js
@@ -27,6 +27,17 @@ if (url.startsWith('https://api.account.meitu.com/users/show_current.json')) {
   response.data.expire_days = -200;
   response.data.vip_tmp_end_time = -1;
   response.data.sub_card_modules = { '02': '200,201,203,204,206,207,208,209,210,211,212,218,400,230,215,213,216,217,220,222,223,224' };
+} else if (url.indexOf('vip/rights/free_info.json') !== -1) {
+  const function_8012 = {
+    function_count: 10000,
+    limit_free_count: 20000,
+    right_count: 0,
+    policy_free_limit: 30000,
+    free_limit_by_member: 30000,
+    member_policy_period: 3,
+    member_policy_free_limit: 40000,
+  }
+  response.data.function_info['8012'] = function_8012;
 }
 
 $done({ body: JSON.stringify(response) });
