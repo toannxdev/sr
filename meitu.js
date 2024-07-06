75a441eb28ff9cb13af52cc8408a994b5c5ce38f
toannxdev <72689204+toannxdev@users.noreply.github.com>
2024-07-06 21:46:48 +0700

    Update meitu.js

--- meitu.js
+++ meitu.js
@@ -1,5 +1,6 @@
 let response = JSON.parse($response.body);
 
 response.response.user.vip.xiuxiu_vip = 100000;
+response.response.user.birthday = '2020-01-01';
 
 $done({ body: JSON.stringify(response) });
