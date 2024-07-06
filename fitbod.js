3829e9a530c72b615c4c0e56d5a44e422c33dca3
toannxdev <72689204+toannxdev@users.noreply.github.com>
2024-07-06 09:58:02 +0700

    update expire time

--- fitbod.js
+++ fitbod.js
@@ -1,7 +1,11 @@
 let response = JSON.parse($response.body);
-response.data.subscriptions[0].expiration_at = 1920579254831;
-response.data.subscriptions[0].expiration_at_date = '2030-07-10T02:40:54.831068Z';
 
-console.log(JSON.stringify(response));
+let date = new Date();
+date.setFullYear(date.getFullYear() + 1000);
+const expiration_at = date.getTime();
+const expiration_at_date = new Date(expiration_at);
+
+response.data.subscriptions[0].expiration_at = expiration_at;
+response.data.subscriptions[0].expiration_at_date = expiration_at_date.toISOString();
 
 $done({ body: JSON.stringify(response) });
