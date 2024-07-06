115e10014b0b733477e543fb297e76883b0c2c86
toannxdev <72689204+toannxdev@users.noreply.github.com>
2024-07-06 08:54:26 +0700

    Update fitbod.js

--- fitbod.js
+++ fitbod.js
@@ -2,4 +2,6 @@ let response = JSON.parse($response.body);
 response.data.subscriptions[0].expiration_at = 1920579254831;
 response.data.subscriptions[0].expiration_at_date = '2030-07-10T02:40:54.831068Z';
 
-$done({body:JSON.stringify(response)});
+console.log(JSON.stringify(response));
+
+$done({ body: JSON.stringify(response) });
