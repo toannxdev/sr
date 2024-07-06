5ded183bbb614e75419fcd85d8d184ed5a3491fe
toannxdev <72689204+toannxdev@users.noreply.github.com>
2024-07-06 21:56:13 +0700

    Update meitu.js

--- meitu.js
+++ meitu.js
@@ -1,6 +1,8 @@
 let response = JSON.parse($response.body);
 
-response.response.user.vip.xiuxiu_vip = 100000;
+response.response.user.vip.xiuxiu_vip = 100;
 response.response.user.birthday = '2020-01-01';
+response.response.user.screen_name = 'Meitu Hacker';
+response.response.user.gender = 'm';
 
 $done({ body: JSON.stringify(response) });
