cecad358f285d65e512e8b79f761922e04faf255
toannxdev <72689204+toannxdev@users.noreply.github.com>
2024-07-24 14:04:15 +0700

    Update lingokids.js

--- lingokids.js
+++ lingokids.js
@@ -2,30 +2,26 @@ let response = JSON.parse($response.body);
 let url = $request.url;
 
 try {
-    if (
-        url.endsWith('/receipts') ||
-        url.endsWith('/auth/provider') ||
-        url.endsWith('/users/profile') ||
-        url.endsWith('/users/identities') ||
-        url.endsWith('/v1/renovate_session')
-    ) {
-        response.info.subscriptions = [{
-            id: 6013463,
-            status: 'trial',
-            state: 'active_trial',
-            in_grace_period: false,
-            product: 'unlimited',
-            platform: 'apple',
-            starts_at: 1721799804,
-            ends_at: 2722404603,
-            plan_id: 'lk.ios.s1m.t7d.p15.v2',
-            duration_unit: 'months',
-            duration_value: 1000,
-            auto_renew_status: true
-        }]
-    }
+  if (url.endsWith('/v1/renovate_session')) {
+    response.info.subscriptions = [
+      {
+        id: 6013463,
+        status: 'trial',
+        state: 'active_trial',
+        in_grace_period: false,
+        product: 'unlimited',
+        platform: 'apple',
+        starts_at: 1721799804,
+        ends_at: 2722404603,
+        plan_id: 'lk.ios.s1m.t7d.p15.v2',
+        duration_unit: 'months',
+        duration_value: 1000,
+        auto_renew_status: true,
+      },
+    ];
+  }
 } catch (error) {
-    print(error);
+  print(error);
 }
 
 $done({ body: JSON.stringify(response) });
